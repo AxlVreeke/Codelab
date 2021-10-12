8fe18e0838ad1725c3031a16ab2637be4f899228
Rooijke <70897974+Rooijke@users.noreply.github.com>
2021-10-12 10:52:15 +0200

    cool

--- about-us.php
+++ about-us.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <body>
-<?php require_once"header.php"?>
+<?php require_once 'header.php';?>
 
 
-<?php require_once"footer.php"?>
+<?php require_once 'footer.php';?>
 </body>
 </html>
