89fd92be2558977d38938919e14045cf49d7cc40
AxlVreeke <D283491@edu.rocwb.nl>
2021-10-12 11:12:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <body>
 <?php require_once 'header.php';?>
-
+<div class="tenor-gif-embed" data-postid="18745496" data-share-method="host" data-aspect-ratio="0.99375" data-width="100%"><a href="https://tenor.com/view/national-coming-out-day-happy-coming-out-day-pride-lgbtq-gif-18745496">National Coming Out Day Happy Coming Out Day GIF</a>from <a href="https://tenor.com/search/national+coming+out+day-gifs">National Coming Out Day GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
 
 <?php require_once 'footer.php';?>
 </body>
