aa64b68bc18884632e7365861f4c403929a7d234
Rooijke <70897974+Rooijke@users.noreply.github.com>
2021-10-12 10:50:11 +0200

    Changes

--- header.php
+++ header.php
@@ -1,10 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Curio petitie</title>
 </head>
-
-</html>
+<header>
+    <nav>
+        <div class="logo">
+            <a href="#">
+                <img src="ing/logo.png" alt="Navigation Logo">
+            </a>
+        <div class="nav-buttons">
+        <a href="#">Home</a>
+        <a href="petitions.php">Petities</a>
+        <a href="about-us.php">Over ons</a>
+        <a href="contact.php">Contact</a>
+        </div>
+        </div>
+    </nav>
+</header>
