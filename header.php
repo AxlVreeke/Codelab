bf5b3daa2260e7ee65db13ab2b3fa5a4129e115b
Rooijke <70897974+Rooijke@users.noreply.github.com>
2021-10-12 11:08:27 +0200

    kaas

--- header.php
+++ header.php
@@ -1,3 +1,10 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Codelab</title>
+</head>
 <header>
     <nav>
         <div class="logo">
